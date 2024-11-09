a9af42c0e2e397115a0fa2572f08c130e47a75c9
prince-gh <pwskills@outlook.com>
2024-11-10 00:00:53 +0530

    Added Content | Added Next & Prev Opt

---
+++ script/module_Ctrl.js
@@ -0,0 +1,222 @@
+import { ModuleData } from  "./user_data.js";
+
+ const urlParams = new URLSearchParams(window.location.search)
+ const Name = urlParams.get('name');
+ const moduleId = Number(urlParams.get('moduleId'));
+
+{
+ const ci0 = document.getElementById('ci0');
+ ci0.addEventListener('click', () => {
+     openContent(Name, 'user.html', 0, 'R');
+ });
+ 
+ const ci1 = document.getElementById('ci1');
+ ci1.addEventListener('click', () => {
+     openContent(Name, 'user.html', 1, 'R');
+ });
+ 
+ const ci2 = document.getElementById('ci2');
+ ci2.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 2, 'Q');
+ });
+ 
+ const ci3 = document.getElementById('ci3');
+ ci3.addEventListener('click', () => {
+     openContent(Name, 'user.html', 3, 'R');
+ });
+ 
+ const ci4 = document.getElementById('ci4');
+ ci4.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 4, 'Q');
+ });
+ 
+ const ci5 = document.getElementById('ci5');
+ ci5.addEventListener('click', () => {
+     openContent(Name, 'user.html', 5, 'R');
+ });
+ 
+ const ci6 = document.getElementById('ci6');
+ ci6.addEventListener('click', () => {
+     openContent(Name, 'user.html', 6, 'R');
+ });
+ 
+ const ci7 = document.getElementById('ci7');
+ ci7.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 7, 'Q');
+ });
+ 
+ const ci8 = document.getElementById('ci8');
+ ci8.addEventListener('click', () => {
+     openContent(Name, 'user.html', 8, 'R');
+ });
+ 
+ const ci9 = document.getElementById('ci9');
+ ci9.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 9, 'Q');
+ });
+ 
+ const ci10 = document.getElementById('ci10');
+ ci10.addEventListener('click', () => {
+     openContent(Name, 'user.html', 10, 'R');
+ });
+ 
+ const ci11 = document.getElementById('ci11');
+ ci11.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 11, 'Q');
+ });
+ 
+ const ci12 = document.getElementById('ci12');
+ ci12.addEventListener('click', () => {
+     openContent(Name, 'user.html', 12, 'R');
+ });
+ 
+ const ci13 = document.getElementById('ci13');
+ ci13.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 13, 'Q');
+ });
+ 
+ const ci14 = document.getElementById('ci14');
+ ci14.addEventListener('click', () => {
+     openContent(Name, 'user.html', 14, 'R');
+ });
+ 
+ const ci15 = document.getElementById('ci15');
+ ci15.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 15, 'Q');
+ });
+ 
+ const ci16 = document.getElementById('ci16');
+ ci16.addEventListener('click', () => {
+     openContent(Name, 'user.html', 16, 'R');
+ });
+ 
+ const ci17 = document.getElementById('ci17');
+ ci17.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 17, 'Q');
+ });
+ 
+ const ci18 = document.getElementById('ci18');
+ ci18.addEventListener('click', () => {
+     openContent(Name, 'user.html', 18, 'R');
+ });
+ 
+ const ci19 = document.getElementById('ci19');
+ ci19.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 19, 'Q');
+ });
+ 
+ const ci20 = document.getElementById('ci20');
+ ci20.addEventListener('click', () => {
+     openContent(Name, 'user.html', 20, 'R');
+ });
+ 
+ const ci21 = document.getElementById('ci21');
+ ci21.addEventListener('click', () => {
+     openContent(Name, 'user.html', 21, 'R');
+ });
+ 
+ const ci22 = document.getElementById('ci22');
+ ci22.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 22, 'Q');
+ });
+ 
+ const ci23 = document.getElementById('ci23');
+ ci23.addEventListener('click', () => {
+     openContent(Name, 'user.html', 23, 'R');
+ });
+ 
+ const ci24 = document.getElementById('ci24');
+ ci24.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 24, 'Q');
+ });
+ 
+ const ci25 = document.getElementById('ci25');
+ ci25.addEventListener('click', () => {
+     openContent(Name, 'user.html', 25, 'R');
+ });
+ 
+ const ci26 = document.getElementById('ci26');
+ ci26.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 26, 'Q');
+ });
+ 
+ const ci27 = document.getElementById('ci27');
+ ci27.addEventListener('click', () => {
+     openContent(Name, 'user.html', 27, 'R');
+ });
+ 
+ const ci28 = document.getElementById('ci28');
+ ci28.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 28, 'Q');
+ });
+ 
+ const ci29 = document.getElementById('ci29');
+ ci29.addEventListener('click', () => {
+     openContent(Name, 'user.html', 29, 'R');
+ });
+ 
+ const ci30 = document.getElementById('ci30');
+ ci30.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 30, 'Q');
+ });
+ 
+ const ci31 = document.getElementById('ci31');
+ ci31.addEventListener('click', () => {
+     openContent(Name, 'user.html', 31, 'R');
+ });
+ 
+ const ci32 = document.getElementById('ci32');
+ ci32.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 32, 'Q');
+ });
+ 
+ const ci33 = document.getElementById('ci33');
+ ci33.addEventListener('click', () => {
+     openContent(Name, 'user.html', 33, 'R');
+ });
+ 
+ const ci34 = document.getElementById('ci34');
+ ci34.addEventListener('click', () => {
+     openContent(Name, 'assignment.html', 34, 'Q');
+ });
+ 
+ const ci35 = document.getElementById('ci35');
+ ci35.addEventListener('click', () => {
+     openContent(Name, 'user.html', 35, 'R');
+ });
+
+}
+
+const modulePrev = document.getElementById('modulePrev');
+modulePrev.addEventListener('click',()=>{
+   
+        let cmid = moduleId-1;
+        let dt = Object.keys(ModuleData[cmid]);
+        console.log(cmid, typeof(cmid), "|",dt, typeof(dt));
+        if(dt=="R"){
+            openContent(Name, 'user.html', cmid, 'R');
+        }else{
+            openContent(Name, 'assignment.html', cmid, 'Q');
+        }
+        
+})
+
+const moduleNext = document.getElementById('moduleNext');
+moduleNext.addEventListener('click',()=>{
+    let cmid = moduleId+1;
+    let dt = Object.keys(ModuleData[cmid]);
+    console.log(cmid, typeof(cmid), "|",dt, typeof(dt));
+
+    if(dt=="R"){
+        openContent(Name, 'user.html', cmid, 'R');
+    }else{
+        openContent(Name, 'assignment.html', cmid, 'Q');
+    }
+})
+
+
+ 
+ function openContent(name, page, moduleId, type){
+    const url = `${page}?moduleId=${moduleId}&type=${type}&name=${Name}`;
+    window.location.href = url;
+}
--- script/user_data.js
+++ script/user_data.js
@@ -1,118 +1,360 @@
 const urlParams = new URLSearchParams(window.location.search)
 const name = urlParams.get('name')
-const result = urlParams.get('result')
-document.getElementById('C_userName').innerHTML = name
+const result = urlParams.get('result') 
+const moduleId = urlParams.get('moduleId');
+const Datatype = urlParams.get('type');
 
-//fetch content from content.json
-// let path = '../dataBase/content.json'
-// fetch(path)
-//   .then(Response => Response.json())
-//   .then(data => {
-//     console.log(data)
-//     renderContent(data)
-//   })
+// DATABASE FOR ALL CONTENT --START
 
-//   .catch(error => alert('Error:', error))
+// const Vdata = [
+  //     {
+    //       "id": "c-1",
+    //       "label": "Mark as Complete",
+    //       "checked": false,
+    //       "videoUrl": "https://drive.google.com/file/d/1FVtAN9ub495ZBMcR_P7L2KiO3_rxd1Fr/preview",
+    
+    //       "thumbnail": "assest/t_n.png"  // Add thumbnail for the first video
+    //   },
+    //   {
+//       "id": "c-2",
+//       "label": "Mark as Complete",
+//       "checked": false,
+//       "videoUrl": "https://drive.google.com/file/d/1BBnYqSQaQSZBieMsOjWFNM8ADgU7hofN/preview",
+//       "thumbnail": "assest/t_n.png"  // Add thumbnail for the second video
+//   },
+// ]
 
-const data = [
-    {
-      "id": "c-1",
-      "label": "Mark as Complete",
-      "checked": false,
-      "videoUrl": "https://drive.google.com/file/d/1FVtAN9ub495ZBMcR_P7L2KiO3_rxd1Fr/preview",
 
-      "thumbnail": "assest/t_n.png"  // Add thumbnail for the first video
-  },
+
+export const ModuleData = [
+  // 0
   {
-      "id": "c-2",
-      "label": "Mark as Complete",
-      "checked": false,
-      "videoUrl": "https://drive.google.com/file/d/1BBnYqSQaQSZBieMsOjWFNM8ADgU7hofN/preview",
-      "thumbnail": "assest/t_n.png"  // Add thumbnail for the second video
+    "R" : {
+      "R1": "010.png",
+      "R2": "011.png"
+    }
   },
-]
-
-renderContent(data)
-
-function renderContent(content) {
-  const contentList = document.getElementById('content-list')
-
-  content.forEach(item => {
-    // Create list item element
-    const li = document.createElement('li')
-
-    // Create div for checkbox and label
-    const div = document.createElement('div')
-    div.classList.add('ctr')
-
-    // Create checkbox input
-    const checkbox = document.createElement('input')
-    checkbox.type = 'checkbox'
-    checkbox.id = item.id
-    checkbox.checked = item.checked
-
-    // Create label
-    const label = document.createElement('i')
-    label.textContent = item.label
-    label.htmlFor = item.id
-
-    // Append checkbox and label to div
-    div.appendChild(checkbox)
-    div.appendChild(label)
-
-    // Create video element
-    const video = document.createElement('iframe')
-    video.width = 310
-    video.height = 200
-    video.src = item.videoUrl
-    video.controls = true  // Add controls for play, pause, etc.
-    video.setAttribute('preload', 'metadata')  // Preload only metadata for faster loading
-    video.setAttribute('loading', 'lazy')  // Lazy loading for better performance
-
-    // Add poster attribute for the video thumbnail
-    video.setAttribute('poster', item.thumbnail)
-
-    // Append video and div to the list item
-    li.appendChild(div)
-    li.appendChild(video)
-
-    // Append list item to the content list
-    contentList.appendChild(li)
-  })
-}
-
-
-document.addEventListener('DOMContentLoaded', renderContent)
-
-document.getElementById('ass').addEventListener('click', e => {
-  const Name = encodeURIComponent(name)
-  window.location.href = `assignment.html?name=${Name}`
-})
-
-if (result === 'Pass') {
-  // Check if result is a string and equals 'pass'
-  const cerButton = document.getElementById('cer')
-  cerButton.style.cursor = 'pointer'
-
-  cerButton.addEventListener('click', e => {
-    const Name = encodeURIComponent(name) // Properly encode the name
-    window.location.href = `certificate.html?name=${Name}` // Use the encoded name
-  })
-} else {
-  const cerButton = document.getElementById('cer')
-  cerButton.style.cursor = 'not-allowed' // Set the cursor to 'not-allowed' when the result is not pass
-}
-
-if (result === 'Pass') { // Check if result is a string and equals 'pass'
-    const cerButton = document.getElementById('cer');
-    cerButton.style.cursor = 'pointer';
-  
-    cerButton.addEventListener('click', e => {
-      const Name = encodeURIComponent(name); // Properly encode the name
-      window.location.href = `certificate.html?name=${Name}`; // Use the encoded name
-    });
-  } else {
-    const cerButton = document.getElementById('cer');
-    cerButton.style.cursor = 'not-allowed'; // Set the cursor to 'not-allowed' when the result is not pass
+  // 1
+  {
+    "R" : {
+      "R1": "110.png",
+      "R2": "111.png"
+    },
+  },
+  // 2
+  {
+    "Q" : [
+      {
+        "question": "What is the full form of URL?",
+        "options": ["Uniform Resource Locator", "Universal Resource Locator", "Uniform Resource Link", "Universal Resource Link"],
+        "answer": "Uniform Resource Locator"
+      },
+   ]
+  },
+  // 3
+  {
+    "R" : {
+      "R1" : "210.png"
+    }
+  },
+  // 4
+  {
+    "Q" : []
+  },
+  // 5
+  {
+    "R" : {
+      "R1" : "310.png"
+    }
+  },
+  // 6
+  {
+    "R" : {
+      "R1" : "410.png"
+    }
+  },
+  // 7
+  {
+    "Q" : []
+  },
+  // 8
+  {
+    "R" : {
+      "R1" : "510.png"
+    }
+  },
+  // 9
+  {
+    "Q" : []
+  },
+  // 10
+  {
+    "R" : {
+      "R1" : "610.png"
+    }
+  },
+  // 11
+  {
+    "Q" : []
+  },
+  // 12
+  {
+    "R" : {
+      "R1" : "710.png"
+    }
+  },
+  // 13
+  {
+    "Q" : []
+  },
+  // 14
+  {
+    "R" : {
+      "R1" : "810.png"
+    }
+  },
+  // 15
+  {
+    "Q" : []
+  },
+  // 16
+  {
+    "R" : {
+      "R1" : "910.png"
+    }
+  },
+  // 17
+  {
+    "Q" : [] 
+  },
+  // 18
+  {
+    "R" : {
+      "R1" : "1010.png"
+    }
+  },
+  // 19
+  {
+    "Q" : []
+  },
+  // 20
+  {
+    "R" : {
+      "R1" : "1110.png"
+    }
+  },
+  // 21
+  {
+    "R" : {
+      "R1" : "1210.png"
+    }
+  },
+  // 22
+  {
+    "Q" : []
+  },
+  // 23
+  {
+    "R" : {
+      "R1" : "1310.png",
+      "R2" : "1311.png",
+      "R3" : "1312.png",
+      "R4" : "1313.png",
+      "R5" : "1314.png",
+      "R6" : "1315.png",
+      "R7" : "1316.png",
+    }
+  },
+  // 24
+  {
+    "Q" : []
+  },
+  // 25
+  {
+    "R" : {
+      "R1" : "1410.png",
+      "R2" : "1411.png",
+      "R3" : "1412.png",
+      "R4" : "1413.png",
+      "R5" : "1414.png",
+      "R6" : "1415.png",
+      "R7" : "1416.png",
+      "R8" : "1417.png",
+      "R9" : "1418.png",
+      "R10" : "1419.png",
+      "R11" : "14110.png"
+    }
+  },
+  // 26
+  {
+    "Q" : []
+  },
+  // 27
+  {
+    "R" : {
+       "R1" : "1510.png",
+      "R2" : "1511.png"
+    }
+  },
+  // 28
+  {
+    "Q" : []
+  },
+  // 29
+  {
+    "R" : {
+    "R1" : "1610.png",
+      "R2" : "1611.png"
+    }
+  },
+  // 30
+  {
+    "Q" : []
+  },
+  // 31
+  {
+    "R" : {
+      "R1" : "1710.png",
+      "R2" : "1711.png"
+    }
+  },
+  // 32
+  {
+    "Q" : []
+  },
+  // 33
+  {
+    "R" : {
+      "R1" : "1810.png",
+      "R2" : "1811.png"
+    }
+  },
+  // 34
+  {
+    "Q" : []
+  },
+  // 35
+  {
+    "R" : {
+   "R1" : "1910.png"
+    }
   }
+];
+
+
+// DATABASE FOR ALL CONTENT --END
+const contentList = document.getElementById('content-list');
+const CurrentMID = document.getElementById('mid');
+
+if(moduleId==null){
+  const url = `${'user.html'}?moduleId=${0}&type=${'R'}&name=${name}`;
+  window.location.href = url;
+}
+
+dataCollector(moduleId,Datatype,ModuleData);
+function dataCollector(moduleId,Datatype,ModuleData){
+  const subModData = ModuleData[moduleId];
+  const finalData = subModData[Datatype];
+  console.log(finalData);
+  if(Datatype==='R'){
+    CurrentMID.innerHTML = moduleId;
+    Object.values(finalData).forEach((e)=>{
+      const li = document.createElement('li');
+      const div = document.createElement('div')
+      const img = document.createElement('img');
+      const path = `moduleDB/M${moduleId}/${e}`;
+      console.log(path);
+      img.width= 320
+      img.src = path;
+      li.appendChild(div)
+      li.appendChild(img)
+      contentList.appendChild(li);
+    })
+  }
+}
+
+
+// renderContent(data)
+
+// function renderContent(content) {
+//   const contentList = document.getElementById('content-list')
+
+//   content.forEach(item => {
+//     // Create list item element
+//     const li = document.createElement('li')
+
+//     // Create div for checkbox and label
+//     const div = document.createElement('div')
+//     div.classList.add('ctr')
+
+//     // Create checkbox input
+//     const checkbox = document.createElement('input')
+//     checkbox.type = 'checkbox'
+//     checkbox.id = item.id
+//     checkbox.checked = item.checked
+
+//     // Create label
+//     const label = document.createElement('i')
+//     label.textContent = item.label
+//     label.htmlFor = item.id
+
+//     // Append checkbox and label to div
+//     div.appendChild(checkbox)
+//     div.appendChild(label)
+
+//     // Create video element
+//     const video = document.createElement('iframe')
+//     video.width = 310
+//     video.height = 200
+//     video.src = item.videoUrl
+//     video.controls = true  // Add controls for play, pause, etc.
+//     video.setAttribute('preload', 'metadata')  // Preload only metadata for faster loading
+//     video.setAttribute('loading', 'lazy')  // Lazy loading for better performance
+
+//     // Add poster attribute for the video thumbnail
+//     video.setAttribute('poster', item.thumbnail)
+
+//     // Append video and div to the list item
+//     li.appendChild(div)
+//     li.appendChild(video)
+
+//     // Append list item to the content list
+//     contentList.appendChild(li)
+//   })
+// }
+
+
+// document.addEventListener('DOMContentLoaded', renderContent)
+
+// document.getElementById('ass').addEventListener('click', e => {
+//   const Name = encodeURIComponent(name)
+//   window.location.href = `assignment.html?name=${Name}`
+// })
+
+// if (result === 'Pass') {
+//   // Check if result is a string and equals 'pass'
+//   const cerButton = document.getElementById('cer')
+//   cerButton.style.cursor = 'pointer'
+
+//   cerButton.addEventListener('click', e => {
+//     const Name = encodeURIComponent(name) // Properly encode the name
+//     window.location.href = `certificate.html?name=${Name}` // Use the encoded name
+//   })
+// } else {
+//   const cerButton = document.getElementById('cer')
+//   cerButton.style.cursor = 'not-allowed' // Set the cursor to 'not-allowed' when the result is not pass
+// }
+
+// if (result === 'Pass') { // Check if result is a string and equals 'pass'
+//     const cerButton = document.getElementById('cer');
+//     cerButton.style.cursor = 'pointer';
+  
+//     cerButton.addEventListener('click', e => {
+//       const Name = encodeURIComponent(name); // Properly encode the name
+//       window.location.href = `certificate.html?name=${Name}`; // Use the encoded name
+//     });
+//   } else {
+//     const cerButton = document.getElementById('cer');
+//     cerButton.style.cursor = 'not-allowed'; // Set the cursor to 'not-allowed' when the result is not pass
+//   }
   
